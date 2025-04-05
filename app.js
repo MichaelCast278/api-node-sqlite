4093f317cfcae0dfa057023f179eeb0a666ffe49
MichaelCast278 <michael.castillo@utec.edu.pe>
2025-04-05 15:05:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.post('/students', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
